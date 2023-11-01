a24b86c3d04f89f72dd74e69165b57c73b8e356a
seif <seifeddine.aaza@gmail.com>
2023-11-01 12:53:29 +0100

    update security protection

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ const limiter = rateLimit({
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Use an external store for consistency across multiple server instances.
 })
 app.use(limiter)
 
