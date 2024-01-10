393150dff5e3a9780da7a8b56ff9d4708789af66
seif <seifeddine.aaza@gmail.com>
2024-01-10 20:48:19 +0100

    client ready to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
-    million.vite({ auto: true }), react()],
+    react(),
+    million.vite({ auto: true })],
 });
  
