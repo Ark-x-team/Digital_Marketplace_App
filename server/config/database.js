a436422c887e0b7863168295476ce87516a728e2
seifeddine aaza <102709884+seifaaza@users.noreply.github.com>
2024-06-24 17:01:42 +0100

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const dbConnect = async () =>  {
-  try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s if MongoDB server is not found
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      retryWrites: true, // Enable automatic retries
-    });
-    console.log("Connected to database");
-  } catch (error) {
-    console.error("Database connection error:", error.message);
+const dbConnect = async (retries = 5) =>  {
+  while (retries) {
+    try {
+      await mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 10000,
+        socketTimeoutMS: 60000,
+        loggerLevel: 'debug',
+      });
+      console.log("Connected to database");
+      break;
+    } catch (error) {
+      console.error("Database connection error:", error.message);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      if (!retries) {
+        throw new Error('Could not connect to the database after several attempts');
+      }
+      await new Promise(res => setTimeout(res, 5000)); // Wait 5 seconds before retrying
+    }
   }
 }
 
